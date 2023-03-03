2070a01f678a1f1e507bedd69c95b62ccea51fcd
Vidyutjammwal <114738096+vidyut46@users.noreply.github.com>
2023-03-03 10:53:37 +0530

    Update palindrome.py

--- problems/palindrome.py
+++ problems/palindrome.py
@@ -2,7 +2,7 @@ string = input("Enter a string: ")
 reversed_string = ""
 for i in string:
     reversed_string = i + reversed_string
-print("Reversed string:", reversed_string)
+print("Reversed string:",(reversed_string))
 if string == reversed_string:
     print("The string is palindrome")
 else:
